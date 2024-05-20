f87daa048cfa7e07e116cbf3b17f3011948b731e
Abdulaziz2888 <164225391+Abdulaziz2888@users.noreply.github.com>
2024-05-20 18:01:06 +0600

    Update BankAccount.java

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,2 +1,12 @@
-package PACKAGE_NAME;public class BankAccount {
+public class LimitException extends Exception {
+    private double remainingAmount;
+
+    public LimitException(String message, double remainingAmount) {
+        super(message);
+        this.remainingAmount = remainingAmount;
+    }
+
+    public double getRemainingAmount() {
+        return remainingAmount;
+    }
 }
