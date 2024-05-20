0ac856f121d47cd8acffe50e054b8f93ee95801c
Abdulaziz2888 <164225391+Abdulaziz2888@users.noreply.github.com>
2024-05-20 18:01:29 +0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        BankAccount account = new BankAccount();
+        account.deposit(15000);
+        System.out.println("На счету: " + account.getAmount() + " сом");
+
+        while (true) {
+            try {
+                account.withDraw(6000);
+                System.out.println("Снято 6000 сом. Остаток: " + account.getAmount() + " сом");
+            } catch (LimitException e) {
+                System.out.println(e.getMessage());
+                System.out.println("Снимаю оставшуюся сумму: " + e.getRemainingAmount() + " сом");
+                try {
+                    account.withDraw((int) e.getRemainingAmount());
+                } catch (LimitException ex) {
+                    // Этот блок не будет выполнен, так как мы снимаем оставшуюся сумму
+                }
+                break;
+            }
+        }
+        System.out.println("Конечный остаток на счете: " + account.getAmount() + " сом");
     }
 }
